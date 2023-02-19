51e2675f1fd5fd2de801978dee0360f5cdf67ad0
WexelSam <samsagbodji@gmail.com>
2023-02-19 09:17:05 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * mainm - Entry point
+ * main - Entry point
  * Return: always 0 (success)
  */
 int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
